db0b6403161ab237bdadf795bcd4b60e7ae66ec8
David Lopez <dlopez@mattmen.com>
2022-04-22 16:35:04 -0400

    OS.js

---
+++ reference/os_demo.js
@@ -0,0 +1,24 @@
+const os = require('os');
+
+// Platform
+// This should console.log the platform that you are using.
+console.log(os.platform());
+
+// CPU Architecture
+// This should console.log the platform that you are using.
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total Memory
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
+
+// UpTime 
+console.log(os.uptime());
