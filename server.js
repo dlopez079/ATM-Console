272ffb95fd17fcfda80b615f6a9ec6b8d07e4ea2
David Lopez <dlopez@mattmen.com>
2022-04-22 14:21:39 -0400

    Server.js. I created the server file so I can serve my project.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFileSync('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html,'});
     res.write(data);
     return res.end();
   });
