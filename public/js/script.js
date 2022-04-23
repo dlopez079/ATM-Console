656c283852f5a51e0f1fae8bb75c1687db508d31
David Lopez <dlopez@mattmen.com>
2022-04-23 13:33:09 -0400

    Server File / Restructured Project Directory. I modified the project folder structure. I confirmed that hte CSS is linked to the html I confirmed that the JS file is linked to the index.html file. I saved some backup files within the project just for reference. Working on the script.js file now.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,168 @@
-// Export person example from this js file.
-// Create the object
-const xObj = {
-  name: "David Lopez", 
-  status: "Success"
+const path = require("path");
+const fs = require("fs");
+
+// Create a function for register
+function loginNavLink() {
+
+  // Change Dyamic Message to welcome
+  document.getElementById('dynamicMessage').innerHTML = "Enter Your Credentials!";
+  displayLoginForm();
+
 }
 
-module.exports = xObj;
+// Create a function for Login
+function registerNavLink() {
+  
+  // Change Dyamic Message to welcome
+  document.getElementById('dynamicMessage').innerHTML = "Please register your account!";
+  displayRegisterForm();
+
+}
+
+// Display Register Form 
+function displayRegisterForm() {
+  // Display register message.
+  let registerForm = document.getElementById('registerForm');
+  let loginForm = document.getElementById('loginForm');
+  
+  registerForm.style.display = "block";
+  loginForm.style.display = "none";
+
+}
+
+// Display Login Form
+function displayLoginForm() { 
+
+  // Save form IDs to a variable.
+  let registerForm = document.getElementById('registerForm');
+  let loginForm = document.getElementById('loginForm');
+  
+  registerForm.style.display = "none";
+  loginForm.style.display = "block";
+
+}
+
+// Submit Button for JSON
+function submit() {
+
+  // Save Value of input fields from form.
+  // let id =+ 1;
+  
+
+  // Check Credentials against JSON Arrays.
+  // loop through json object to see if the user exists.
+
+  // Display the Users json.
+  
+}
+
+// Create function to add users to JSON.
+function loginUser() {
+
+  // Capture the values from the form fields.
+  let username = document.getElementById('username').value;
+  
+  console.log(username)
+   
+  // If user does not exist, ask them to register.
+  // if(username != users.username) {
+  //   // Change Dyamic Message to welcome
+  //   document.getElementById('dynamicMessage').innerHTML = "Check Spelling or register your account!";
+  //   displayRegisterForm();
+  // }
+  // else check user pin.
+
+}
+
+
+
+
+
+
+
+
+// Create function to add users to JSON.
+function registerUser() {
+
+  // Capture the values from the form fields.
+  let firstName = document.getElementById('getFirstName').value;
+  let lastName = document.getElementById('getLastName').value;
+  let username = document.getElementById('getUsername').value;
+  let pin = document.getElementById('getPin').value;
+  
+  console.log(firstName + lastName + username + pin);
+  
+  // Retreive Users JSON.
+  fs.readFile(path.join(__dirname, '../json', 'users.json'), 'utf8', (err,data) => {
+    if(err) throw err; // If there is an error, this function will let you know.
+    console.log(data); // Verify that I can pull the data.  
+  });
+
+}
+
+module.exports = registerUser;
+
+
+
+
+
+
+
+
+
+
+
+function transfer() {
+  // Retrieve all accounts for logged in user.
+
+  // Select the "FROM" account.
+
+  // Select the "TO" account.
+
+  // Save Transfer Amount from DOM.
+  let transferAmount = document.getElementById('transferAmount').value
+
+  // Retreive the balance of the "FROM" account and subtract the amount you want to transfer.
+
+  // Retreive the balance of the "TO" account and add the amount you want to transfer.
+}
+
+
+
+
+function depositAmount() {
+  // Retrieve Balance
+  // Loop through the table
+  let currentBalance = accounts[0].balance;
+
+  let deposit = parseInt(document.getElementById("depositAmt").value);
+
+  // 1000.00 + 100.00
+  let newBalance = currentBalance + deposit;
+  console.log(newBalance)
+
+  // Change Balance
+  accounts[0].balance = newBalance;
+  console.table(accounts);
+
+  document.getElementById('balanceAmt').innerHTML = newBalance;
+  document.getElementById("depositAmt").value = "";
+}
+
+function withdrawAmount() {
+  // Retrieve Balance
+  let currentBalance = accounts[0].balance;
+
+  let withdraw = parseInt(document.getElementById("withdrawAmt").value);
+
+  // 1000.00 + 100.00
+  let newBalance = currentBalance - withdraw;
+  console.log(newBalance)
+
+  // Change Balance
+  accounts[0].balance = newBalance;
+  console.table(accounts);
+
+  document.getElementById('balanceAmt').innerHTML = newBalance;
+  document.getElementById("withdrawAmt").value = "";
+}
--- server.js
+++ server.js
@@ -2,51 +2,68 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
+// Request the server and get a response
 const server = http.createServer((req, res) => {
 
-  // Route to index.html
-  if (req.url === "/") {
+  // Builid File Path for the public folder that you created on the root directory(__dirname)
+  let filePath = path.join(
+    __dirname,
+    "public",
+    // If the URL equals '/' then you are going to load the index.html or request the HMTL file that has the same name of the route. Example (localhost:5000/index.html will pull the index.html/localhost:5000/about will fetch the about.html).
+    req.url === "/" ? "index.html" : req.url  
+  );
 
-    // Read the index.html
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err; // If there is an error, this function will let you know.
+  // Save the Extension of the files that is within the public folder and save it to variable.
+  let extname = path.extname(filePath);
 
-        res.writeHead(200, { "Content-Type": "text/html" }); // Set tht type of the file that will display.
-        res.end(content);
+  // We have to tell the server what the content type of the file is.  (If it's an html file, then the content type is 'text/html'; if it's css, then the content type is 'text/css', etc.)
+  let contentType = "text/html";
+
+  // Use a switch to determine the content type for the files that live in your public folder using the Extension of the file which is saved to a variable already.
+  switch (extname) {
+    case ".js":
+      contentType = "application/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // Read the file that is located in the public folder.
+  fs.readFile(filePath, (err, content) => {
+
+    // If there is an err, we will check for the 'ENOENT = Error NO ENTity/ENTry'
+    if (err) {
+      if (err.code == "ENOENT") {
+
+        // Display a 'Page not found' html which is located inthe public folder.
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        )
+      } else {
+        // Or the Default Server Error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
       }
-    );
-  }
-
-  // ROUTE FOR ABOUT
-  if (req.url === "/about") {
-
-    // Read the ABOUT.html
-    fs.readFile(
-      path.join(__dirname, "public", "about.html"),
-      (err, content) => {
-        if (err) throw err; // If there is an error, this function will let you know.
-
-        res.writeHead(200, { "Content-Type": "text/html" }); // Set tht type of the file that will display.
-        res.end(content);
-      }
-    );
-  }
-
-  // ROUTE TO DEVIVERY API USING JUST NODE.
-  if (req.url === "/api/users") {
-
-    // Fetch data from a data base.
-    const users = [
-      { name: 'David Lopez', age: 42 },
-      { name: 'David Lopez', age: 42 }
-    ];
-
-    res.writeHead(200, { "Content-Type": "text/html" }); // Set tht type of the file that will display.
-    res.end(JSON.stringify(users));
-  }
-
+    } else {
+      // If there is no error, then mark 200 code (success) and display html file.
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
